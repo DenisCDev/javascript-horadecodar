7c21a25e971fac32f7fc3e1a066727696a95c8f7
Denis Scarabelli <87149177+DenisCDev@users.noreply.github.com>
2023-02-19 08:08:57 -0300

    Aula 6 concluida

---
+++ 5_number/js/script.js
@@ -0,0 +1,19 @@
+var idade = 19;
+
+console.log(idade);
+console.log(typeof idade);
+
+var numeroVirgula = 9.43;
+
+console.log(numeroVirgula);
+console.log(typeof numeroVirgula); //ainda é number
+
+var textoComNumero = "645";
+
+console.log(textoComNumero);
+console.log(typeof textoComNumero); //vira string
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
---
+++ 6_string/js/script.js
@@ -0,0 +1,22 @@
+var nome = "Denis";
+var sobrenome = "Scarabelli"
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "teste");
+
+console.log(nomeCompleto);
+
+console.log(typeof "2.532453");
+
+var frase = "Estou estudando JavaScript";
+console.log(frase);
+console.log(typeof frase);
+
+document.write("Olá ");
+document.write("Ele disse 'Olá'");
+
+console.log("Ele se chama " + nome);
