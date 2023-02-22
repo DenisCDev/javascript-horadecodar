ec245c3ed0560769c01d604a0d15cc82993dab29
Denis Scarabelli <87149177+DenisCDev@users.noreply.github.com>
2023-02-22 09:34:53 -0300

    Aula 10 feita

---
+++ 10_arrays/js/script.js
@@ -0,0 +1,16 @@
+var array = [1,2,3,4];
+console.log(array);
+
+var nomes = ["Denis", "Lethicia", "Nina"];
+console.log(nomes);
+
+console.log(nomes[0]);
+console.log(nomes[2]);
+
+//inserir elementos
+
+nomes[2] = "Eveline";
+
+nomes[3] = "Matheus";
+
+console.log(nomes)
