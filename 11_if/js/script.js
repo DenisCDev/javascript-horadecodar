1b757d0f18b647008a4f9fc9d3f89ea654d8f414
Denis Scarabelli <87149177+DenisCDev@users.noreply.github.com>
2023-03-14 16:24:48 -0300

    Aula 11 concluida sobre if

---
+++ 11_if/js/script.js
@@ -0,0 +1,12 @@
+var idade = 19;
+var maiorDeIdade = 18;
+
+console.log("Vamos avaliar sua idade");
+
+if(idade >= maiorDeIdade) {
+    console.log("Pode fazer a carteira de habilitação");
+}
+
+if(idade < maiorDeIdade) {
+    console.log("Você ainda é menor de idade");
+}
