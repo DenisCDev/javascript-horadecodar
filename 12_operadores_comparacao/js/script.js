a68e421f93e4c6f1a5e84b9627a99498a0080a5c
Denis Scarabelli <87149177+DenisCDev@users.noreply.github.com>
2023-03-14 17:58:44 -0300

    Aula 12 concluida operadores de comparação

---
+++ 12_operadores_comparacao/js/script.js
@@ -0,0 +1,18 @@
+var idade = 19;
+var militarAtivo = false;
+
+if(idade >= 18) {
+    console.log("O usuário tem que se apresentar ou já ter se apresentado no exercito");
+}
+
+if(idade <= 17) {
+    console.log("O usuário ainda vai ter que se apresentar no exercito");
+}
+
+if(militarAtivo == true) {
+    console.log("O usuário já está sevindo como militar");
+}
+
+if(militarAtivo == false) {
+    console.log("O usuário não está servindo como militar");
+}
