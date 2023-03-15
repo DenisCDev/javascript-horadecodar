5e83e2982d11048594201ed4102fe620a1e4d6ed
Denis Scarabelli <87149177+DenisCDev@users.noreply.github.com>
2023-03-15 20:56:41 -0300

    Aula 13 concluida sobre else if

---
+++ 13_else_if/js/script.js
@@ -0,0 +1,9 @@
+var nome = "Denis";
+
+if(nome == "Denis") {
+    console.log("Entrada autorizada");
+} else if(nome == "Lethicia"){
+    console.log("Entrada autorizada para o setor B");
+} else {
+    console.log("Auto-destruição em 3, 2, 1...");
+}
