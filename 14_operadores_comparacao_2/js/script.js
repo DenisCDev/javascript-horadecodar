eba73cc6b4d3751ee3d61bc23177fcf49986d91b
Denis Scarabelli <87149177+DenisCDev@users.noreply.github.com>
2023-03-16 17:46:06 -0300

    Até a aula 15 concluido, operadores logicos

---
+++ 14_operadores_comparacao_2/js/script.js
@@ -0,0 +1,11 @@
+var aniversario = 2;
+
+//Apenas number é valido na validação abaixo
+
+if(aniversario === 2) {
+    console.log("É o aniversário do Denis");
+}
+
+if(aniversario !== 2) {
+    console.log("Não é o aniversário do tipo number");
+}
---
+++ 15_operadores_logicos/js/script.js
@@ -0,0 +1,14 @@
+let usuario = "denis";
+let senha = "senha123";
+
+if(usuario == "denis" && senha == "senha123") {
+    console.log("Login efetuado");
+} else {
+    console.log("Usuário e/ou senha incorretos, tente novamente");
+}
+
+if(1 == 1 && 9>2 && true) {
+    console.log("Passou");
+} else {
+    console.log("Não passou");
+}
